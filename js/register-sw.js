301cc5dc8df7baccff70e74a34f2067a260a33e9
Benjamin Simonson <bfsimon@gmail.com>
2018-11-16 17:09:24 -0500

    registers service worker

---
+++ js/register-sw.js
@@ -0,0 +1,11 @@
+// Register service worker only if supported
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', {scope: '/'})
+    .then(reg => {
+      // registration worked
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch(error => {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  }
