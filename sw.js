9e4a458d7d8c8b33faf14440994aacc9bedc866c
Benjamin Simonson <bfsimon@gmail.com>
2018-11-17 17:45:37 -0500

    fixes restaurant details offline accessibility

--- sw.js
+++ sw.js
@@ -51,6 +51,17 @@ self.addEventListener('activate', event => {
 
 // Hijack fetch requests
 self.addEventListener('fetch', event => {
+  const requestUrl = new URL(event.request.url);
+
+  // Hijack only requests made to our app, not external requests
+  if (requestUrl.origin === location.origin) {
+    // Requests to restaurant.html include search parameters
+    // which doesn't allow the URL to be used as the key to access the cache
+    // instead respondWith restaurant.html if the requested pathname starts with '/restaurant.html'
+    if (requestUrl.pathname.startsWith('/restaurant.html')) {
+      event.respondWith(caches.match('restaurant.html'));
+    }
+  }
   // Respond with cached data, if any, falling back to network
   event.respondWith(
     caches.match(event.request).then(response => {
