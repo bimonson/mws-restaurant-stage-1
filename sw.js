67d111ba59583461e540016bd5104cf9e74a1e4e
Benjamin Simonson <bfsimon@gmail.com>
2018-11-17 19:40:05 -0500

    caches responsive images

--- sw.js
+++ sw.js
@@ -60,6 +60,13 @@ self.addEventListener('fetch', event => {
     // instead respondWith restaurant.html if the requested pathname starts with '/restaurant.html'
     if (requestUrl.pathname.startsWith('/restaurant.html')) {
       event.respondWith(caches.match('restaurant.html'));
+      return;
+    }
+
+    // If the requested pathname starts with '/img' call serveImg
+    if (requestUrl.pathname.startsWith('/img')) {
+      event.respondWith(serveImg(event.request));
+      return;
     }
   }
   // Respond with cached data, if any, falling back to network
@@ -69,3 +76,20 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
+function serveImg(request) {
+  let imgUrl = request.url;
+
+  // Create a new URL with the suffix and file extension removed
+  // This will be the key for storing the image into the cache
+  imgUrl = imgUrl.replace(/-small-1x\.\w{3}|-small-2x\.\w{3}|-medium-1x\.\w{3}|-medium-2x\.\w{3}|-large-1x\.\w{3}|-large-2x\.\w{3}/i, '');
+
+  return caches.open(contentImgCache).then(cache => {
+    return cache.match(imgUrl).then(response => {
+      return response || fetch(request).then(networkResponse => {
+        cache.put(imgUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
